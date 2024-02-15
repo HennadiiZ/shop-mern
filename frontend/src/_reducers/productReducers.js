61f570eb3eef019f77275511c73391fa68a328dd
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2024-02-14 19:11:33 -0700

    Product details Reducer and Action

--- frontend/src/_reducers/productReducers.js
+++ frontend/src/_reducers/productReducers.js
@@ -2,6 +2,9 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from '../constants/productConstants';
 
 const initialState = {
@@ -16,6 +19,24 @@ export const productListReducer = (state = initialState, action) => {
     case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload };
     case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+const initialProductState = {
+  product: { reviews: [] },
+  //   product: {},
+};
+
+export const productDetailsReducer = (state = initialProductState, action) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true, ...state };
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, ...state, product: action.payload };
+    case PRODUCT_DETAILS_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
--- frontend/src/_store/store.js
+++ frontend/src/_store/store.js
@@ -1,10 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { thunk } from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { productListReducer } from '../_reducers/productReducers';
+import {
+  productListReducer,
+  productDetailsReducer,
+} from '../_reducers/productReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 
 const initialState = {};
