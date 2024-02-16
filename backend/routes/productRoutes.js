fc60c52763e18aa0519da404ba08b5a68d56dcfb
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2024-02-16 12:09:37 -0700

    clean up comments

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -11,34 +11,12 @@ import {
 // @route GET /api/products
 // @access Public
 
-router.get(
-  '/',
-  getProducts()
-  // asyncHandler(async (req, res) => {
-  //   // const products = await Product.find({});
-  //   // res.json(products);
-  // })
-);
+router.get('/', getProducts());
 
 // @desc Fetch single product
 // @route GET /api/products/:id
 // @access Public
 
-router.get(
-  '/:id',
-  getProductById()
-  // asyncHandler(async (req, res) => {
-  //   // const product = await Product.findById(req.params.id);
-
-  //   // if (product) {
-  //   //   res.json(product);
-  //   // } else {
-  //   //   // res.status(404).json({ message: 'Product not found' });
-
-  //   //   res.status(404);
-  //   //   throw new Error('Product not found');
-  //   // }
-  // })
-);
+router.get('/:id', getProductById());
 
 export default router;
