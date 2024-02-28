99ea0f1450792010f1df075ded72f5bb48ce424a
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2024-02-28 01:16:45 -0700

    LoginPage and RegisterPage

--- frontend/src/pages/LoginPage/LoginPage.js
+++ frontend/src/pages/LoginPage/LoginPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { Form, Button, Row, Col } from 'react-bootstrap';
+import { Link as RouterLink, useLocation, useNavigate } from 'react-router-dom';
+import { Button, Typography, TextField, Grid } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../../components/Message/Message';
 import Loader from '../../components/Loader/Loader';
@@ -21,12 +21,6 @@ const LoginPage = () => {
 
   const redirect = location.search ? location.search.split('=')[1] : '/';
 
-  //   useEffect(() => {
-  //     if (userInfo) {
-  //       history.push(redirect);
-  //     }
-  //   }, [history, userInfo, redirect]);
-
   useEffect(() => {
     if (userInfo) {
       navigate(redirect);
@@ -40,45 +34,132 @@ const LoginPage = () => {
 
   return (
     <FormContainer>
-      <h1>Sign In</h1>
+      <Typography variant='h4'>Sign In</Typography>
       {error && <Message variant='danger'>{error}</Message>}
       {loading && <Loader />}
-      <Form onSubmit={submitHandler}>
-        <Form.Group controlId='email'>
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control
-            type='email'
-            placeholder='Enter email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+      <form onSubmit={submitHandler}>
+        <TextField
+          label='Email Address'
+          variant='outlined'
+          fullWidth
+          margin='normal'
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-        <Form.Group controlId='password'>
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Enter password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+        <TextField
+          label='Password'
+          variant='outlined'
+          fullWidth
+          margin='normal'
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-        <Button type='submit' variant='primary' className='mt-3'>
+        <Button
+          type='submit'
+          variant='contained'
+          color='primary'
+          fullWidth
+          sx={{ mt: 3 }}
+        >
           Sign In
         </Button>
-      </Form>
+      </form>
 
-      <Row className='py-3'>
-        <Col>
+      <Grid container justifyContent='center' className='py-3'>
+        <Typography>
           New Customer?{' '}
-          <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+          <RouterLink
+            to={redirect ? `/register?redirect=${redirect}` : '/register'}
+          >
             Register
-          </Link>
-        </Col>
-      </Row>
+          </RouterLink>
+        </Typography>
+      </Grid>
     </FormContainer>
   );
 };
 
 export default LoginPage;
+
+// import React, { useState, useEffect } from 'react';
+// import { Link, useLocation, useNavigate } from 'react-router-dom';
+// import { Form, Button, Row, Col } from 'react-bootstrap';
+// import { useDispatch, useSelector } from 'react-redux';
+// import Message from '../../components/Message/Message';
+// import Loader from '../../components/Loader/Loader';
+// import FormContainer from '../../components/FormContainer/FormContainer';
+// import { login } from '../../_actions/userActions';
+
+// const LoginPage = () => {
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+
+//   const location = useLocation();
+//   const navigate = useNavigate();
+
+//   const dispatch = useDispatch();
+
+//   const userLogin = useSelector((state) => state.userLogin);
+//   const { loading, error, userInfo } = userLogin;
+
+//   const redirect = location.search ? location.search.split('=')[1] : '/';
+
+//   useEffect(() => {
+//     if (userInfo) {
+//       navigate(redirect);
+//     }
+//   }, [navigate, userInfo, redirect]);
+
+//   const submitHandler = (e) => {
+//     e.preventDefault();
+//     dispatch(login(email, password));
+//   };
+
+//   return (
+//     <FormContainer>
+//       <h1>Sign In</h1>
+//       {error && <Message variant='danger'>{error}</Message>}
+//       {loading && <Loader />}
+//       <Form onSubmit={submitHandler}>
+//         <Form.Group controlId='email'>
+//           <Form.Label>Email Address</Form.Label>
+//           <Form.Control
+//             type='email'
+//             placeholder='Enter email'
+//             value={email}
+//             onChange={(e) => setEmail(e.target.value)}
+//           ></Form.Control>
+//         </Form.Group>
+
+//         <Form.Group controlId='password'>
+//           <Form.Label>Password</Form.Label>
+//           <Form.Control
+//             type='password'
+//             placeholder='Enter password'
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)}
+//           ></Form.Control>
+//         </Form.Group>
+
+//         <Button type='submit' variant='primary' className='mt-3'>
+//           Sign In
+//         </Button>
+//       </Form>
+
+//       <Row className='py-3'>
+//         <Col>
+//           New Customer?{' '}
+//           <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+//             Register
+//           </Link>
+//         </Col>
+//       </Row>
+//     </FormContainer>
+//   );
+// };
+
+// export default LoginPage;
