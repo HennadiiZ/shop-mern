e047ac99970bdff4cfd0f43d3c3ba20ffce9e670
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2024-02-28 01:08:19 -0700

    ProductPage update

--- frontend/src/pages/ProductPage/ProductPage.js
+++ frontend/src/pages/ProductPage/ProductPage.js
@@ -50,6 +50,8 @@ const ProductPage = () => {
             padding: 3,
             marginTop: 1,
             borderRadius: 2,
+            boxShadow:
+              '0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 2px rgba(0, 0, 0, 0.24)',
           }}
         >
           <Grid item md={3}>
