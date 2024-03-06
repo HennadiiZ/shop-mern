b1d431f2c5d6c0570f339b0a042eac64a3027a91
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2024-03-05 22:17:03 -0700

    Refactor Loader

--- frontend/src/components/Loader/Loader.js
+++ frontend/src/components/Loader/Loader.js
@@ -1,21 +1,40 @@
 import React from 'react';
-import { Spinner } from 'react-bootstrap';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 const Loader = () => {
   return (
-    <Spinner
-      animation='border'
-      role='status'
-      style={{
-        width: '100px',
-        height: '100px',
-        margin: 'auto',
-        display: 'block',
-      }}
+    <Box
+      display='flex'
+      justifyContent='center'
+      alignItems='center'
+      minHeight='100vh' // Adjust the height as needed
     >
-      <span className='sr-only'>Loading...</span>
-    </Spinner>
+      <CircularProgress size={100} />
+    </Box>
   );
 };
 
 export default Loader;
+
+// import React from 'react';
+// import { Spinner } from 'react-bootstrap';
+
+// const Loader = () => {
+//   return (
+//     <Spinner
+//       animation='border'
+//       role='status'
+//       style={{
+//         width: '100px',
+//         height: '100px',
+//         margin: 'auto',
+//         display: 'block',
+//       }}
+//     >
+//       <span className='sr-only'>Loading...</span>
+//     </Spinner>
+//   );
+// };
+
+// export default Loader;
